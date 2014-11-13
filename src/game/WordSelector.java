03b4a5ade26ba2c233c6cd4c3e68ed003890da7f
Vincent Montalieu <vincent.montalieu@outlook.com>
2014-11-13 17:28:04 +0100

    WordSelector is a class that randomly select a word among the word.txt file The file has to be located next to src folder

--- src/game/WordSelector.java
+++ src/game/WordSelector.java
@@ -1,5 +1,58 @@
 package game;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.LineNumberReader;
+import java.util.Random;
+
 public class WordSelector {
 
+    public static int countLines() {
+	LineNumberReader lnr = null;
+	try {
+	    lnr = new LineNumberReader(new FileReader(new File("words.txt")));
+	} catch (FileNotFoundException e) {
+	    e.printStackTrace();
+	}
+	try {
+	    lnr.skip(Long.MAX_VALUE);
+	} catch (IOException e1) {
+	    e1.printStackTrace();
+	}
+	int nbLines = lnr.getLineNumber();
+	try {
+	    lnr.close();
+	} catch (IOException e) {
+	    e.printStackTrace();
+	}
+	return nbLines;
+    }
+
+    private static int random() {
+	Random rand = new Random();
+	return (int) (Math.random() * ((countLines() + 1)));
+    }
+
+    @SuppressWarnings("resource")
+    public static String getWord() {
+	BufferedReader in = null;
+	try {
+	    in = new BufferedReader(new FileReader(new File("words.txt")));
+	} catch (FileNotFoundException e) {
+	    e.printStackTrace();
+	}
+	String line = null;
+	int max = random();
+	for (int i = 0; i <= max; i++) {
+	    try {
+		line = in.readLine();
+	    } catch (IOException e) {
+		e.printStackTrace();
+	    }
+	}
+	return line;
+    }
 }
